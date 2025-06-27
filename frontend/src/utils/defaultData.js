911fda58ad9a7d8d44181d7fba934a5da42c0d21
AI Project <himajach01@gmail.com>
2025-06-27 15:41:49 +0530

    Clean project setup: Interview AI (Frontend + Backend)

---
+++ frontend/src/utils/defaultData.js
@@ -0,0 +1,22 @@
+export const features = [
+ 
+  {
+    title: "Interview With AI",
+    title2: "Interview",
+    description:
+      "Interactive dialogue exploring technology, innovation, and AI-driven insights",
+    icon: "/assets/letter.png",
+    link: "/user/live-interview",
+    name: 'Live Interview'
+  },
+  {
+    title: "AI Career Coach",
+    title2: "AI Coach",
+    description:
+      "Guiding AI enthusiasts toward successful, fulfilling careers in technology.",
+    icon: "/assets/network.png",
+    link: "/user/ai-career-coatch",
+    name: 'AI Career Coatch'
+  },
+ 
+];
