a7138bd9699e094c0d04f0d7206fe6f88ecee2b6
Himaja <22b01a4618@svecw.edu.in>
2025-07-19 14:59:32 +0530

    Removed duplicate backend and frontend folders

--- Interview_AI-backend-main/src/models/shareLinkschema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const sharedInterviewLinkSchema = new mongoose.Schema({
-  candidateEmail: { type: String, required: true, unique: true },
-  googleMeetLink: { type: String, required: true },
-});
-
-module.exports = mongoose.model('SharedInterviewLink', sharedInterviewLinkSchema);
